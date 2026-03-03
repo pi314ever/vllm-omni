75fac78c7285b0de5bc0b70b368bccffaffda1c9
Daniel Huang <daniel1.huang@intel.com>
2026-03-03 12:38:52 -0800

    Add test stage configs for xpu
    Signed-off-by: Daniel Huang <daniel1.huang@intel.com>

--- tests/e2e/offline_inference/test_qwen2_5_omni.py
+++ tests/e2e/offline_inference/test_qwen2_5_omni.py
@@ -39,6 +39,9 @@ if current_omni_platform.is_npu():
 elif current_omni_platform.is_rocm():
     # ROCm stage config optimized for MI325 GPU
     stage_config = str(Path(__file__).parent.parent / "stage_configs" / "rocm" / "qwen2_5_omni_ci.yaml")
+elif current_omni_platform.is_xpu():
+    # ROCm stage config optimized for MI325 GPU
+    stage_config = str(Path(__file__).parent.parent / "stage_configs" / "xpu" / "qwen2_5_omni_ci.yaml")
 else:
     stage_config = get_cuda_graph_config()
 
