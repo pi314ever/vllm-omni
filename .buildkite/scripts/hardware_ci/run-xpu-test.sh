c65b6656dcd2d0ff44e9538fb122c17d98ec9fc8
Daniel Huang <daniel1.huang@intel.com>
2026-03-04 20:19:38 -0800

    Explicitly list all passing files for Intel XPU CI
    Signed-off-by: Daniel Huang <daniel1.huang@intel.com>

--- .buildkite/scripts/hardware_ci/run-xpu-test.sh
+++ .buildkite/scripts/hardware_ci/run-xpu-test.sh
@@ -48,19 +48,55 @@ time timeout -k 30 50m docker run \
     echo $ZE_AFFINITY_MASK
     pip install tblib==3.1.0
     cd /workspace/vllm-omni
-    pytest -v -s tests/ \
-        --ignore=tests/benchmarks/test_serve_cli.py \
-        --ignore=tests/e2e/offline_inference/test_bagel_text2img.py \
-        --ignore=tests/e2e/offline_inference/test_qwen2_5_omni.py \
-        --ignore=tests/e2e/offline_inference/test_qwen3_omni.py \
-        --ignore=tests/e2e/offline_inference/test_sequence_parallel.py \
-        --ignore=tests/e2e/offline_inference/test_t2v_model.py \
-        --ignore=tests/e2e/online_serving/test_qwen3_omni.py \
-        --ignore=tests/e2e/online_serving/test_qwen3_omni_expansion.py \
-        --ignore=tests/examples/online_serving/test_qwen3_omni.py \
-        -k "not test_ring_p2p"
+    pytest -v -s \
+        tests/benchmarks/patch/test_patch.py \
+        tests/comfyui/test_comfyui_integration.py \
+        tests/diffusion/cache/test_cache_backends.py \
+        tests/diffusion/distributed/test_cfg_parallel.py \
+        tests/diffusion/distributed/test_hsdp.py \
+        tests/diffusion/distributed/test_parallel_state_sp_groups.py \
+        tests/diffusion/distributed/test_sp_plan_hooks.py \
+        tests/diffusion/distributed/test_vae_patch_parallel.py \
+        tests/diffusion/lora/test_base_linear.py \
+        tests/diffusion/lora/test_lora_manager.py \
+        tests/diffusion/models/nextstep_1_1/test_nextstep_cfg_parallel_layout.py \
+        tests/diffusion/models/z_image/test_zimage_tp_constraints.py \
+        tests/diffusion/test_diffusers_loader.py \
+        tests/diffusion/test_diffusion_model_runner.py \
+        tests/diffusion/test_diffusion_worker.py \
+        tests/diffusion/test_multiproc_executor_concurrency.py \
+        tests/diffusion/test_worker_wrapper_base.py \
+        tests/distributed/omni_connectors/test_adapter_and_flow.py \
+        tests/distributed/omni_connectors/test_chunk_transfer_adapter.py \
+        tests/distributed/omni_connectors/test_kv_flow.py \
+        tests/distributed/omni_connectors/test_mooncake_transfer_engine_buffer.py \
+        tests/distributed/omni_connectors/test_mooncake_transfer_engine_rdma.py \
+        tests/distributed/omni_connectors/test_omni_connector_configs.py \
+        tests/e2e/offline_inference/test_cache_dit.py \
+        tests/e2e/offline_inference/test_t2i_model.py \
+        tests/e2e/offline_inference/test_zimage_parallelism.py \
+        tests/e2e/online_serving/test_images_generations_lora.py \
+        tests/engine/test_async_omni_engine_abort.py \
+        tests/entrypoints/openai_api/test_image_server.py \
+        tests/entrypoints/openai_api/test_serving_chat_metrics.py \
+        tests/entrypoints/openai_api/test_serving_chat_sampling_params.py \
+        tests/entrypoints/openai_api/test_serving_speech.py \
+        tests/entrypoints/openai_api/test_video_server.py \
+        tests/entrypoints/test_async_omni_diffusion_config.py \
+        tests/entrypoints/test_cfg_companion_tracker.py \
+        tests/entrypoints/test_omni_diffusion.py \
+        tests/entrypoints/test_omni_input_preprocessor.py \
+        tests/entrypoints/test_omni_llm.py \
+        tests/entrypoints/test_omni_new_request_data.py \
+        tests/entrypoints/test_omni_stage_diffusion_config.py \
+        tests/entrypoints/test_stage_utils.py \
+        tests/entrypoints/test_utils.py \
+        tests/metrics/test_stats.py \
+        tests/model_executor/models/qwen2_5_omni/test_audio_length.py \
+        tests/model_executor/models/qwen2_5_omni/test_qwen2_5_omni_embed.py \
+        tests/model_executor/models/qwen3_tts/test_cuda_graph_decoder.py \
+        tests/test_outputs.py \
+        tests/worker/test_gpu_generation_model_runner.py \
+        tests/worker/test_omni_gpu_model_runner.py \
+        tests/worker/test_process_gpu_memory.py
 '
-
-# Test as of 5295f0f81b28a353a03fd242a77de043392372b0
-# tests/diffusion/distributed/test_comm.py::test_ring_p2p hangs, but other tests in that file pass.
-# All other ignored tests hang the CI indefinitely.
